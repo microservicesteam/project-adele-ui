ea0dc8fe275673a4e2a6b23435e25b9702d231f8
Balazs Szogi <balazs_szogi@epam.com>
2017-01-25 20:59:09 +0100

    Added basic socket.io server

---
+++ server/server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;
+
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
+});
+
+var debug = false;
+
+debug && console.log("Server Started");
+io.sockets.on('connection', function (socket) {
+  debug && console.log("New Connection");
+
+  socket.on('message', function(msg){
+    debug && console.log("New msg");
+    io.sockets.emit('message', msg);
+  });
+
+});
