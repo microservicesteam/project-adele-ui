3149f8a682b43282230b0035bff7fe2ff90edaab
Andras Kantor <and.kantor@gmail.com>
2017-09-06 20:15:15 +0200

    Sector selection works

--- react-seed/app/components/Booking/Sector.jsx
+++ react-seed/app/components/Booking/Sector.jsx
@@ -4,13 +4,20 @@ import PropTypes from "prop-types";
 
 export default class Sector extends React.Component {
 
+  onClick = () => {
+    this.props.setSelected(this.props.sector.id);
+  };
+
   render() {
     return (
-      <div className={styles.sector}>{this.props.sector.id}</div>
+      <div className={[styles.sector, this.props.selected ? styles.active : styles.inactive].join(' ')}
+           onClick={this.onClick}>{this.props.sector.id}</div>
     );
   }
 }
 
 Sector.propTypes = {
-  sector: PropTypes.any.isRequired
+  sector: PropTypes.any.isRequired,
+  selected: PropTypes.any.isRequired,
+  setSelected: PropTypes.any.isRequired
 };
--- react-seed/app/components/Booking/SectorList.jsx
+++ react-seed/app/components/Booking/SectorList.jsx
@@ -5,12 +5,29 @@ import Sector from "./Sector";
 
 export default class SectorList extends React.Component {
 
+  constructor() {
+    super();
+    this.state = {
+      selected: null
+    };
+  }
+
+  setSelected = (id) => {
+    this.setState({
+      selected: id
+    })
+  };
+
   render() {
     return (
       <div>
         <div className={styles.header}>Sectors</div>
         <div>
-          {this.props.sectors.map((sector) => <Sector sector={sector} key={'sector-' + sector.id} />)}
+          {this.props.sectors.map((sector) =>
+            <Sector key={'sector-' + sector.id}
+                    sector={sector}
+                    selected={this.state.selected == sector.id}
+                    setSelected={this.setSelected}/>)}
         </div>
       </div>
     );
